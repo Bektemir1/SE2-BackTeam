13fa516a935adcc657cd8d47f007f1c76f3a9ce9
Damirbek1997 <damirbek.abdybapuulu@iaau.edu.kg>
2020-03-31 18:43:38 +0600

    authorization with jwt 2

--- backend/src/main/java/com/example/demo/connectDB/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.connectDB;
-
-import com.example.demo.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-    Optional<Users> findByUsername(String username);
-}
